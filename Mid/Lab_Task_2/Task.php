2d99c6e69e42bc182c91d46ce51173d544b96027
FairoozAlamMahi <23-51878-2@student.aiub.edu>
2025-11-16 13:52:45 +0600

    Firs form with validation

--- Mid/Lab_Task_2/Task.php
+++ Mid/Lab_Task_2/Task.php
@@ -1,11 +1,98 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    helloowoowow
+  <title>Labask2</title>
+
+  <style>
+   
+
+    h2 {
+      text-align: center;
+    }
+
+form {
+      background-color:white;
+      padding: 20px;
+      width: 300px;
+      margin: 0 auto;
+    }
+
+     input {
+      width: 90%;
+      padding: 10px;
+      margin-top: 10px;
+    }
+
+    button {
+      background-color: #003366;
+      color: white;
+      cursor: pointer;
+      padding: 10px;
+      margin-top: 10px;
+    }
+
+#output {
+      margin-top: 20px;
+      text-align: center;
+      font-size: 16px;
+      color: #003366;
+    } </style>
+   </head>
+<body style="background-color:lightblue">
+
+<center>  <h2>Sudent Registration</h2> </center>
+
+  <form onsubmit="return submithandle()">
+
+    <label>Full Name:</label>
+    <input type="text" id="name">
+
+    <label>Email:</label>
+    <input type="email" id="email">
+
+    <label>Password:</label>
+    <input type="password" id="password">
+
+    <label>Confirm Password:</label>
+    <input type="password" id="confirmPassword">
+
+    <button type="submit">Register</button>
+  </form>
+
+  <div id="output"></div>
+
+  <script>
+    function submithandle() {
+      var name = document.getElementById("name").value.trim();
+      var email = document.getElementById("email").value.trim();
+      var password = document.getElementById("password").value;
+      var confirmPassword = document.getElementById("confirmPassword").value;
+
+      var outputDiv = document.getElementById("output");
+
+      outputDiv.innerHTML = "";
+
+      if (name === "" || email === "" || password === "" || confirmPassword === "") {
+        alert("Please fill all the fields");
+        return false;
+      }
+
+      if (password !== confirmPassword) {
+        alert("Password didnt match!");
+        return false;
+      }
+
+      outputDiv.innerHTML = `
+        <b>Registration Successful!</b><br><br>
+        Name: ${name}<br>
+        Email: ${email}<br>
+      `;
+
+      return false;
+    }
+  </script>
+
+
+
 </body>
 </html>
