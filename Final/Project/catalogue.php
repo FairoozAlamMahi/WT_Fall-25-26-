47dfe4f7d5ae93f32741e254e8d508442469a009
FairoozAlamMahi <23-51878-2@student.aiub.edu>
2025-12-20 10:26:55 +0600

    added boxes

--- Final/Project/catalogue.php
+++ Final/Project/catalogue.php
@@ -5,17 +5,116 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>catalogue</title>
     <link rel="stylesheet" href="CSS/catalogue.css">
-
+    <script src="JS/catalogue.js" defer></script>
 </head>
 
 <body>
-     <img src="images/logo.png" id="logo">
+
+    <img src="images/logo.png" id="logo">
+
     <h1>Catalogue</h1>
-    <center><p>Pick the car of your dreams</p></center>
+
+    <div id="selector">
+        <a href="#" id="familyBtn">Family Cars</a> | 
+        <a href="#" id="sportsBtn">Sports Cars</a>
+    </div>
+
+
+    <!-- FAMILY CARS SECTION -->
+    <div id="family">
+        
+        <div class="family-box">
+            <img src="images/car.png">
+            <h3>Car Name</h3>
+            <p>Price: $$$$</p>
+            <p>Specs: 120HP, Auto</p>
+        </div>
+
+        <div class="family-box">
+            <img src="images/car.png">
+            <h3>Car Name</h3>
+            <p>Price: $$$$</p>
+            <p>Specs: 120HP, Auto</p>
+        </div>
+
+        <div class="family-box">
+            <img src="images/car.png">
+            <h3>Car Name</h3>
+            <p>Price: $$$$</p>
+            <p>Specs: 120HP, Auto</p>
+        </div>
+
+        <div class="family-box">
+            <img src="images/car.png">
+            <h3>Car Name</h3>
+            <p>Price: $$$$</p>
+            <p>Specs: 120HP, Auto</p>
+        </div>
+
+        <div class="family-box">
+            <img src="images/car.png">
+            <h3>Car Name</h3>
+            <p>Price: $$$$</p>
+            <p>Specs: 120HP, Auto</p>
+        </div>
+
+        <div class="family-box">
+            <img src="images/car.png">
+            <h3>Car Name</h3>
+            <p>Price: $$$$</p>
+            <p>Specs: 120HP, Auto</p>
+        </div>
+
+        <div class="family-box">
+            <img src="images/car.png">
+            <h3>Car Name</h3>
+            <p>Price: $$$$</p>
+            <p>Specs: 120HP, Auto</p>
+        </div>
+
+        <div class="family-box">
+            <img src="images/car.png">
+            <h3>Car Name</h3>
+            <p>Price: $$$$</p>
+            <p>Specs: 120HP, Auto</p>
+        </div>
+
+    </div>
 
 
 
+    <!-- SPORTS CARS SECTION -->
+    <div id="sports" style="display:none;">
+
+        <div class="sports-box">
+            <img src="images/car.png">
+            <h2>Sports Car Name</h2>
+            <p>Price: $$$$</p>
+            <p>Specs: 500HP, Manual</p>
+        </div>
+
+        <div class="sports-box">
+            <img src="images/car.png">
+            <h2>Sports Car Name</h2>
+            <p>Price: $$$$</p>
+            <p>Specs: 500HP, Manual</p>
+        </div>
+
+        <div class="sports-box">
+            <img src="images/car.png">
+            <h2>Sports Car Name</h2>
+            <p>Price: $$$$</p>
+            <p>Specs: 500HP, Manual</p>
+        </div>
+
+        <div class="sports-box">
+            <img src="images/car.png">
+            <h2>Sports Car Name</h2>
+            <p>Price: $$$$</p>
+            <p>Specs: 500HP, Manual</p>
+        </div>
+
+    </div>
 
 </body>
-
 </html>
