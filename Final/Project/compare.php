48f927a24dfb04f6661fe8e06994d6766fcc5242
FairoozAlamMahi <23-51878-2@student.aiub.edu>
2025-12-20 14:32:42 +0600

    added new page compare. and added logo and usericon to that

---
+++ Final/Project/compare.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Compare</title>
+    <link rel="stylesheet" href="CSS/compare.css">
+</head>
+<body>
+
+
+    <a href="dashboard.php">
+    <img src="images/logo.png" id="logo">
+    </a>
+
+
+    <a href="user1.html">
+    <img src="images/user1.png" id="usericon"
+         style="width:60px; height:60px; position:absolute; top:10px; right:70px; z-index:1000; border:3px solid red;">
+    </a>
+
+
+</body>
+</html>
