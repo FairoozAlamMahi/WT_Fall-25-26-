cd0ce2d249067a57efb6e8c39cd0e7fe248fdc92
FairoozAlamMahi <23-51878-2@student.aiub.edu>
2025-12-26 11:49:03 +0600

    Added user and footer to marketplace

---
+++ Final/Project/marketplace.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+
+
+
+
+
+
+
+
+
+<a href="user1.html">
+    <img src="images/user1.png" id="usericon"
+         style="width:60px; height:60px; position:absolute; top:10px; right:70px; z-index:1000; border:3px solid red;">
+</a>
+
+
+<div id="footer" style="
+       text-align: center;
+    color: white;
+    margin-top: 100px;
+    padding: 20px;
+    font-size: 14px;
+    opacity: 0.7;
+">
+    <p>© 2025 NG Auto. All rights reserved.</p>
+    <p>Contact: support@ngauto.com | +880-111-222-333</p>
+</div>
+
+
+</body>
+</html>
