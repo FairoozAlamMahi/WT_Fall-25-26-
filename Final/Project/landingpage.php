c0e1550d40b80c47bf11e9df0b7ee5b6e8abf7e0
FairoozAlamMahi <23-51878-2@student.aiub.edu>
2026-01-02 12:15:58 +0600

    landing page. added the usuals

---
+++ Final/Project/landingpage.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="CSS/landingpage.css">
+</head>
+<body>
+    
+    <!-- LOGO -->
+<a href="dashboard.php">
+    <img src="images/logo.png" id="logo">
+</a>
+
+<a href="user1.html">
+    <img src="images/user1.png" id="usericon"
+         style="width: 60px;
+    height: 60px;
+    position: absolute;
+    top: 10px;
+    right: 70px;">
+</a>
+
+
+
+<!-- FOOTER -->
+<div id="footer">
+    <p>© 2025 NG Auto. All rights reserved.</p>
+    <p>Contact: support@ngauto.com | +880-111-222-333</p>
+</div>
+
+
+</body>
+</html>
