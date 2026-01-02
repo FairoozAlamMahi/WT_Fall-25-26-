8399c16814705d82f5b3305e51643eec0d74e94c
FairoozAlamMahi <23-51878-2@student.aiub.edu>
2026-01-03 00:50:14 +0600

    finally was able to add db correctly to catalogue

--- Final/Project/catalogue.php
+++ Final/Project/catalogue.php
@@ -1,135 +1,87 @@
+<?php
+include __DIR__ . "/DB/db.php";
+
+/* FETCH FAMILY CARS */
+$familyCars = [];
+$familyResult = $conn->query("SELECT * FROM cars WHERE category='Family'");
+if ($familyResult && $familyResult->num_rows > 0) {
+    while ($row = $familyResult->fetch_assoc()) {
+        $familyCars[] = $row;
+    }
+}
+
+/* FETCH SPORTS CARS */
+$sportsCars = [];
+$sportsResult = $conn->query("SELECT * FROM cars WHERE category='Sports'");
+if ($sportsResult && $sportsResult->num_rows > 0) {
+    while ($row = $sportsResult->fetch_assoc()) {
+        $sportsCars[] = $row;
+    }
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>catalogue</title>
+    <title>Catalogue</title>
     <link rel="stylesheet" href="CSS/catalogue.css">
     <script src="JS/catalogue.js" defer></script>
 </head>
 
 <body>
+
 <a href="dashboard.php">
     <img src="images/logo.png" id="logo">
 </a>
 
-    <h1>Catalogue</h1>
+<h1>Catalogue</h1>
 
-    <div id="selector">
-        <a href="#" id="familyBtn">Family Cars</a> | 
-        <a href="#" id="sportsBtn">Sports Cars</a>
-    </div>
-
-
+<div id="selector">
+    <a href="#" id="familyBtn">Family Cars</a> | 
+    <a href="#" id="sportsBtn">Sports Cars</a>
+</div>
 
 <a href="user1.html">
     <img src="images/user1.png" id="usericon"
-         style="width: 60px;
-    height: 60px;
-    position: absolute;
-    top: 10px;
-    right: 70px;">
+         style="width:60px; height:60px; position:absolute; top:10px; right:70px;">
 </a>
 
-
-
-
-<!-- Family CARS SECTION -->
-    <div id="family">
-    <div class="family-box">
-        <img src="images/2020-toyota-corolla-sedan.jpg">
-        <h3>Toyota Corolla</h3>
-        <p>Price: 2500000</p>
-        <p>139 HP • 1.8L • Automatic</p>
-    </div>
-
-    <div class="family-box">
-        <img src="images/honda civic.jpg">
-        <h3>Honda Civic</h3>
-        <p>Price: 2400000</p>
-        <p>158 HP • 2.0L • Automatic</p>
-    </div>
-
-    <div class="family-box">
-        <img src="images/2019-toyota-rav4.jpg">
-        <h3>Toyota RAV4</h3>
-        <p>Price: 2800000</p>
-        <p>203 HP • 2.5L • AWD</p>
-    </div>
-
-    <div class="family-box">
-        <img src="images/nissan.jpg">
-        <h3>Nissan X-Trail</h3>
-        <p>Price: 2700500</p>
-        <p>169 HP • 2.5L • CVT</p>
-    </div>
-
-    <div class="family-box">
-        <img src="images/10816_suzuki_swift_505_r1.jpg">
-        <h3>Suzuki Swift</h3>
-        <p>Price: 1500000</p>
-        <p>82 HP • 1.2L • Manual</p>
-    </div>
-
-    <div class="family-box">
-        <img src="images/2024_Mazda.jpg">
-        <h3>Mazda CX-5</h3>
-        <p>Price: 1229000</p>
-        <p>187 HP • 2.5L • Automatic</p>
-    </div>
-
-    <div class="family-box">
-        <img src="images/kia-sportage-review.jpg">
-        <h3>Kia Sportage</h3>
-        <p>Price: 1227000</p>
-        <p>181 HP • 2.4L • Automatic</p>
-    </div>
-
-    <div class="family-box">
-        <img src="images/2024_Mazda.jpg">
-        <h3>Mazda CX-5 (Alt)</h3>
-        <p>Price: 1229000</p>
-        <p>188 HP • 2.5L • Automatic</p>
-    </div>
-
+<!-- FAMILY CARS -->
+<div id="family">
+<?php
+if (!empty($familyCars)) {
+    foreach ($familyCars as $car) {
+        echo "
+        <div class='family-box'>
+            <img src='images/cars/{$car['image']}'>
+            <h3>{$car['brand']} {$car['model']}</h3>
+            <p>Price: {$car['price']}</p>
+            <p>{$car['horsepower']} HP • {$car['engine_capacity']} • {$car['transmission']}</p>
+        </div>
+        ";
+    }
+}
+?>
 </div>
 
-
-
-    <!-- SPORTS CARS SECTION -->
-    <div id="sports" style="display:none;">
-
-    <div class="sports-box">
-        <img src="images/gtr.jpg">
-        <h2>Nissan GT-R</h2>
-        <p>Price: 12113000</p>
-        <p>565 HP • 3.8L V6 • AWD</p>
-    </div>
-
-    <div class="sports-box">
-        <img src="images/2018_Ford_Mustang_GT_5.0.jpg">
-        <h2>Ford Mustang GT</h2>
-        <p>Price: 1255000</p>
-        <p>450 HP • 5.0L V8 • Manual</p>
-    </div>
-
-    <div class="sports-box">
-        <img src="images/Toyota_Supra.jpg">
-        <h2>Toyota Supra</h2>
-        <p>Price: 1250000</p>
-        <p>382 HP • 3.0L Turbo • Automatic</p>
-    </div>
-
-    <div class="sports-box">
-        <img src="images/Porsche-992-Carrera-s-001.jpg">
-        <h2>Porsche 911 Carrera</h2>
-        <p>Price: 12100000</p>
-        <p>379 HP • 3.0L Twin-Turbo • RWD</p>
-    </div>
-
+<!-- SPORTS CARS -->
+<div id="sports" style="display:none;">
+<?php
+if (!empty($sportsCars)) {
+    foreach ($sportsCars as $car) {
+        echo "
+        <div class='sports-box'>
+            <img src='images/cars/{$car['image']}'>
+            <h2>{$car['brand']} {$car['model']}</h2>
+            <p>Price: {$car['price']}</p>
+            <p>{$car['horsepower']} HP • {$car['engine_capacity']} • {$car['transmission']}</p>
+        </div>
+        ";
+    }
+}
+?>
 </div>
-
-
 
 <div id="footer">
     <p>© 2025 NG Auto. All rights reserved.</p>
